6a99e2ba1c8617ed00a5610123e9b88cf2b7c5df
mohab ghobashy <93479326+MohabGhobashy@users.noreply.github.com>
2023-02-27 07:28:45 +0200

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,71 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QPixmap>
+#include <QImage>
+#include <QVector>
+#include <QQueue>
+#include "opencv2/world.hpp"
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+using namespace cv;
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+String currentFileName;
+Mat dest;
+
+//take path from uploader
+
+QString imgPath = QFileDialog::getOpenFileName(this, "Open an Image", "..", "Images (*.png *.xpm *.jpg *.bmb)");
+//read image using opencv
+
+Mat image = imread(imgPath.toStdString());
+cvtColor(image, dest,COLOR_BGR2RGB);
+
+//convert image to greyscale
+Mat grey;
+cvtColor(image, grey,COLOR_BGR2GRAY);
+
+//convert opencv_photo to Qimage to show it in qt
+QImage image2((uchar*)dest.data, dest.cols, dest.rows,QImage::Format_RGB888);
+QPixmap pix = QPixmap::fromImage(image2);
+int width_img=ui->label_img->width();
+int height_img=ui->label_img->height();
+ui->label_img->setPixmap(pix.scaled(width_img,height_img,Qt::KeepAspectRatio));
+
+
+QImage image3((uchar*)grey.data, grey.cols, grey.rows,QImage::Format_Grayscale8);
+QPixmap pix3 = QPixmap::fromImage(image3);
+int width_img3=ui->grey_image->width();
+int height_img3=ui->grey_image->height();
+ui->grey_image->setPixmap(pix3.scaled(width_img3,height_img3,Qt::KeepAspectRatio));
+
+
+
+//Mat threshold;
+//cv::threshold(grey,threshold,244, 255, THRESH_BINARY);
+}
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+
+}
+
---
+++ openCvProj.pro
@@ -0,0 +1,31 @@
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+CONFIG += c++17
+
+# You can make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+INCLUDEPATH += C:\opencv\build\include
+LIBS += C:\opencv\open-cv-build\bin\libopencv_calib3d460.dll
+LIBS += C:\opencv\open-cv-build\bin\libopencv_core460.dll
+LIBS += C:\opencv\open-cv-build\bin\libopencv_highgui460.dll
+LIBS += C:\opencv\open-cv-build\bin\libopencv_imgcodecs460.dll
+LIBS += C:\opencv\open-cv-build\bin\libopencv_imgproc460.dll
+
+
+SOURCES += \
+    main.cpp \
+    mainwindow.cpp
+
+HEADERS += \
+    mainwindow.h
+
+FORMS += \
+    mainwindow.ui
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /opt/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
